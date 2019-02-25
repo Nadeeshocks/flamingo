a9ab8f580b42b6162fe8ef09e18ae66b538052ff
Nadia Iqbal <nadee.shocks@gmail.com>
2019-02-25 23:59:03 +0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React ,{ Component } from 'react';
+import ReactDOM from 'react-dom';
+import App from './js/App';
+import './css/index.css';
+
+console.log("hey there");
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
---
+++ webpack.common.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+module.exports = {
+  entry : path.join(__dirname, './src/index.js'),
+
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    chunkFilename: '[name].chunk.js',
+  },
+
+  optimization: {
+    splitChunks: {
+      chunks: 'all', 
+    },
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+      path: path.resolve(__dirname, 'build'),
+    }),
+    new CleanWebpackPlugin(['build']),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './public/index.html',
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: [/\.css$/, /\.scss$/],
+        use: [
+          "style-loader",
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          "css-loader"
+        ]
+      },
+      // transpiles JSX and ES6
+      {
+        test: [/\.js?$/],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+
+      }
+    ]
+  }
+};
+
+
+// script  "start": "webpack-dev-server --open --config webpack.dev.js "
