0d76637cbc0d39d0b442a7274943ff2e19f00af9
Nadeeshocks <nadee.shocks@gmail.com>
2019-03-23 11:47:04 +0500

    Update product.js

--- src/js/components/product/product.js
+++ src/js/components/product/product.js
@@ -5,17 +5,19 @@ import ColorPalette from '../colorPalette/color_palette';
 
 const Product = ({url , product, selectedColor, colorPalette ,onClickHandle }) =>{  
   const renderImage = () => {
-    product.img.map(item =>{
-      item.type == selectedColor ? <img src={item.url} alt="product image"/> :""
+    // console.log(product);
+    return product.img.map(item =>{
+      console.log(item.type === selectedColor);
+      return item.type == selectedColor ? <img src={item.url} alt="product image"/> :""
     })       
   }
    return  <Col sm="4" className="mb-5 text-center">
       <Link to={`${url}/${product.id}`}>
         <div className="product-img">
           {            
-            // product.colors ? renderImage() : <img src={product.img} alt="product image"/>
+            product.colors ? renderImage() : <img src={product.img[0].url} alt="product image"/>
           }
-          <img src={product.img[0].url} alt="product image"/>
+          {/* <img src={product.img[0].url} alt="product image"/> */}
         </div>
         <div className="product-detail">
         <h3 className="text-uc-12">{product.product_title}</h3>
